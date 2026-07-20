7cc7bb951ef01ab32d712d0dbc008dddf858d73c
JosephPadovano <jp2397@njit.edu>
2026-07-20 16:31:05 -0400

    Add solution code for all files

---
+++ lib/project_api.php
@@ -0,0 +1,29 @@
+<?php
+// UCID: jp2397, Date: 7/13/26
+require_once(__DIR__ . "/api_helper.php");
+function fetch_phone_data($search, $source, &$errors = []){
+    if ($source === "sample") {
+            $result = api_sample_response("project-api-sample.json");
+    } 
+    else {
+        $result = api_get(
+        "https://gsmarenaparser.p.rapidapi.com/api/values/clean/devices",
+        ["search" => $search],
+        ["key_name" => "RAPIDAPI_KEY", "host_name" => "RAPIDAPI_HOST"]
+        );
+    }
+    $decoded = decode_api_response($result, "devices", $errors);
+    $mapped_phones = [];
+    if (isset($decoded["devices"])){
+        foreach ($decoded["devices"] as $phone){
+            $mapped_phones[] = [
+                "phone_brand" => $phone["manufacturer"] ?? "Unknown",
+                "phone_model" => $phone["model"] ?? "Unknown",
+                "screen_size" => $phone["displaysize"] ?? "Unknown",
+                "camera_megapixels" => $phone["main_camera_mp"] ?? "Unknown"
+            ];
+        }
+    }
+    return $mapped_phones;
+}
+?>
