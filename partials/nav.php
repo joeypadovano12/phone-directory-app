a4d9b5fbefd8c344184f46506a07d4891e79a634
JosephPadovano <jp2397@njit.edu>
2026-08-06 11:43:17 -0400

    Add assign_phones file and code

--- partials/nav.php
+++ partials/nav.php
@@ -17,6 +17,7 @@ $isLoggedIn = is_logged_in();
                 <li><a href="<?php echo project_url('admin/list_phones.php'); ?>">List Phones</a></li>
                 <li><a href="<?php echo project_url('admin/all_saved_phones.php'); ?>">All Saved Phones</a></li>
                 <li><a href="<?php echo project_url('admin/unassociated_phones.php'); ?>">Unassociated Phones</a></li>
+                <li><a href="<?php echo project_url('admin/assign_phones.php'); ?>">Assign Phones</a></li>
                 <li><a href="<?php echo project_url('admin/create_role.php'); ?>">Create Role</a></li>
                 <li><a href="<?php echo project_url('admin/list_roles.php'); ?>">List Roles</a></li>
                 <li><a href="<?php echo project_url('admin/assign_roles.php'); ?>">Assign Roles</a></li>
